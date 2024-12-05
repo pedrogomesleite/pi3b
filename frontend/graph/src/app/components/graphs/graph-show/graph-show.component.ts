0ad8b295af90acd7d679016f16a6fbc5a1a601db
PitzTech <victorlaurentino7@gmail.com>
2024-12-05 20:57:51 -0300

    fix code

--- frontend/graph/src/app/components/graphs/graph-show/graph-show.component.ts
+++ frontend/graph/src/app/components/graphs/graph-show/graph-show.component.ts
@@ -1,18 +1,18 @@
-import {Component, OnInit} from '@angular/core';
-import {GraphService} from "../../../services/graph.manipulation.service";
-import {NodeDto} from "../../../entitys/node/node.dto";
-import {SharedGraphService} from "../../../services/shared-graph.service";
-import {MatExpansionModule} from '@angular/material/expansion';
-import {Node2d} from "../../../entitys/node/node.2d";
-import {Config2d} from "./config/2d-config";
-import {Node3d} from "../../../entitys/node/node.3d";
-import {Config3d} from "./config/3d-config";
-import {NgForOf, NgIf} from "@angular/common";
-import {MatChipsModule} from "@angular/material/chips";
-import {HttpClient} from "@angular/common/http";
-import {ActivatedRoute} from "@angular/router";
-import {api} from "../../../api";
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import { Component, OnInit } from '@angular/core';
+import { GraphService } from "../../../services/graph.manipulation.service";
+import { NodeDto } from "../../../entitys/node/node.dto";
+import { SharedGraphService } from "../../../services/shared-graph.service";
+import { MatExpansionModule } from '@angular/material/expansion';
+import { Node2d } from "../../../entitys/node/node.2d";
+import { Config2d } from "./config/2d-config";
+import { Node3d } from "../../../entitys/node/node.3d";
+import { Config3d } from "./config/3d-config";
+import { NgForOf, NgIf } from "@angular/common";
+import { MatChipsModule } from "@angular/material/chips";
+import { HttpClient } from "@angular/common/http";
+import { ActivatedRoute } from "@angular/router";
+import { api } from "../../../api";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-graph-show',
@@ -72,12 +72,13 @@ export class GraphShowComponent implements OnInit {
     });
   }
 
-
+  sessionId?: number;
   async fetchGrafo() {
     await this.http.get<Object[]>(api + 'sessoes').forEach((sessoes: any[]) => {
       for (let sesso of sessoes) {
         if (sesso['grupo_id'] === this.id) {
-          this.connectWebSocket(sesso['conexao'], );
+          this.sessionId = sesso['id'];
+          this.connectWebSocket(sesso['conexao'],);
         }
       }
     }).catch(() => {
@@ -87,7 +88,7 @@ export class GraphShowComponent implements OnInit {
 
   async connectWebSocket(conexao: string) {
     console.log(conexao + 0)
-    this.websocket = new WebSocket(conexao + '0');
+    this.websocket = new WebSocket(conexao + `?session_id=${this.sessionId}&observer=true`);
 
     this.websocket.onopen = () => {
       this.websocket?.send('labirinto');
@@ -95,6 +96,7 @@ export class GraphShowComponent implements OnInit {
 
     this.websocket.onmessage = async (event) => {
       const data = event.data;
+
       console.log('Mensagem recebida:', data);
       const dataString: string = event.data;
       if (dataString.includes("Labirinto atual: ")) {
@@ -130,7 +132,11 @@ export class GraphShowComponent implements OnInit {
 
         }).then();
       }
+
+      console.log({ dataString })
       if (dataString.startsWith("[")) {
+        console.log(dataString)
+
         const list: number[] = JSON.parse(dataString);
 
         for (const listKey in list) {
